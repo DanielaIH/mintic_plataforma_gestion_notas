3903221e26ed8894601e8b6314be2e1e1c15c68d
DanielaIH <4427@holbertonschool.com>
2022-09-21 21:10:40 -0500

    ADD new templates by Nataly and Daniela

---
+++ login.js
@@ -0,0 +1,18 @@
+function validarformulario(){
+    let usuario=document.getElementById("txtUsuario").value;
+    let password=document.getElementById("txtPassword").value;
+    let expresion= /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+
+    if(usuario.length == 0 || usuario.length <8){
+        alert("Ingresar el nombre del usuario");
+                
+    }
+ 
+ 
+ if (password.length==0 || password.length <8){
+    alert("Ingresar clave correcta")
+    
+ }
+ }    
+
+
